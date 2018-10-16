daf6451191053609acc2395c0f555e5309393b54
macoba69 <carlos.matelunab@gmail.com>
2018-10-16 11:20:35 -0300

    Add from for messages and show incoming messages in browser

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
@@ -15,19 +16,15 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: 'Jhon',
-        text: 'See you then',
-        createAt: 123123
-    });
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+    
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-    socket.on('createMessage', (message) => {
+       
+    socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        });
+        io.emit('newMessage', generateMessage(message.from, message.text));
+        callback('This from the server.');        
     });
 
     socket.on('disconnect', () => {
